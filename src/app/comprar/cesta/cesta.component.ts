1924ee2ddd622931bc836a397c27e2990a7c1d9c
Marina Benito <marina.benito@edu.uah.es>
2022-04-16 12:05:12 +0200

    la calma antes de la tempestad

--- src/app/comprar/cesta/cesta.component.ts
+++ src/app/comprar/cesta/cesta.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CestaService } from 'src/app/cesta.service';
 
 @Component({
   selector: 'app-cesta',
   templateUrl: './cesta.component.html',
-  styleUrls: ['./cesta.component.css']
+  styleUrls: ['./cesta.component.css'],
 })
 export class CestaComponent implements OnInit {
+  constructor(public _cestaService: CestaService) {}
+  cesta: any = this._cestaService.getProductosCesta();
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  resetCesta() {
+    localStorage.setItem('cesta', JSON.stringify([]));
   }
 
+  getPrecioTotal() {
+    var precioTotal = 0;
+    this.cesta.forEach(function (producto: any) {
+      precioTotal += producto.precio * producto.cantidad;
+    });
+    return precioTotal;
+  }
+
+  getNumArticulos() {
+    var numArticulosCesta = 0;
+    this.cesta.forEach(function (producto: any) {
+      numArticulosCesta += producto.cantidad;
+    });
+    return numArticulosCesta;
+  }
 }
--- src/app/comprar/cesta/producto-cesta/producto-cesta.component.ts
+++ src/app/comprar/cesta/producto-cesta/producto-cesta.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { CestaService } from 'src/app/cesta.service';
 
 @Component({
   selector: 'app-producto-cesta',
@@ -8,7 +9,12 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./producto-cesta.component.css'],
 })
 export class ProductoCestaComponent implements OnInit {
-  constructor() {}
+  constructor(public _cestaService: CestaService) {}
   faTrash = faTrash;
+  @Input() prod: any;
   ngOnInit(): void {}
+
+  deleteCesta() {
+    this._cestaService.deleteProductoCesta(this.prod);
+  }
 }
